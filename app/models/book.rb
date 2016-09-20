cd9240df4a30dfaffc944525d66f6531652879e9
mindziukasss <mindziukasss@gmail.com>
2016-09-20 16:58:13 +0300

    Added ability to review books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
+	has_many :reviews
 
 
 	  has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
